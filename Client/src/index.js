2fef9eb903e439a8c2923d86df447ae7a3c91176
asracedof <116568718+asracedof@users.noreply.github.com>
2023-04-29 12:13:00 -0500

    29/04 URL TB

--- Client/src/index.js
+++ Client/src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './redux/store';
+const base_url = process.env.REACT_APP_BASE_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>  
+    <BrowserRouter basename={base_url}>  
     <App />,
     </BrowserRouter>
    </Provider>
